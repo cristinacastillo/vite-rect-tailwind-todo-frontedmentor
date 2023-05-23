c7cd2caf6faf16accd9ef02075916e1edae8acf1
CRISTINA CASTILLO OBREGON <0017286@viewnext.com>
2023-05-23 12:53:56 +0200

    dia 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
 import TodoCreate from "./components/TodoCreate";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
 
-const initialStateTodos = [
+/* const initialStateTodos = [
   {
     id: 1,
     title: "Complete online Js bluuweb curse",
@@ -26,10 +26,15 @@ const initialStateTodos = [
     title: "Completar toda la aplicación",
     completed: false,
   },
-];
+]; */
+const initialStateTodos = JSON.parse(localStorage.getItem("todos") || []);
 
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const createTodo = (title) => {
     const newTodo = {
@@ -79,10 +84,15 @@ const App = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] ">
+    <div
+      className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] 
+    bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]
+     md:bg-[url('./assets/images/bg-desktop-light.jpg')]
+     md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] "
+    >
       <Header />
 
-      <main className="container mx-auto mt-8 px-4">
+      <main className="container mx-auto mt-8 px-4 md:max-w-xl">
         <TodoCreate createTodo={createTodo} />
 
         <TodoList
